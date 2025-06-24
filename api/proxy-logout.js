070b3b0d3fbe8bc93ea44638d76fffbc7c40ef04
Server-test-line <emailserverapp113@gmail.com>
2025-06-24 14:11:40 +0800

    Create proxy-logout.js

---
+++ api/proxy-logout.js
@@ -0,0 +1,6 @@
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method Not Allowed' });
+  }
+  return res.status(200).json({ message: 'Logout successful' });
+}
