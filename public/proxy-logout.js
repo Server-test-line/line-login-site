65980182ad3bc6f83b7a1d41b8451b92ed150ccb
Server-test-line <emailserverapp113@gmail.com>
2025-06-24 14:09:18 +0800

    Update proxy-logout.js

--- public/proxy-logout.js
+++ public/proxy-logout.js
@@ -1,11 +1,7 @@
-export async function logout() {
+async function logout() {
   try {
     await fetch('/api/logout', { method: 'POST' });
-
-    // 清除登入資訊
     localStorage.removeItem('userName');
-
-    // 導回登入頁面
     window.location.href = 'index.html';
   } catch (error) {
     console.error('登出失敗', error);
